a86a4ce54d2b59687ac85d6d5beddbf15089c595
Kees van Voorthuizen <me@keesvv.nl>
2022-07-04 23:15:42 +0200

    feat(wasm): add wee_alloc & kernel buffer

--- src/arch/wasm32/mod.rs
+++ src/arch/wasm32/mod.rs
@@ -1,13 +1,28 @@
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 pub mod chr {}
+
 pub mod interrupt {
     pub enum Interrupts {}
 }
+
 pub mod kernel {
-    pub fn log_fmt(_: core::fmt::Arguments) {}
+    extern crate alloc;
+    use alloc::vec::Vec;
+    use spin::Mutex;
+
+    pub static KERNEL_BUFFER: Mutex<Vec<&str>> = Mutex::new(Vec::new());
+
+    pub fn log_fmt(args: core::fmt::Arguments) {
+        KERNEL_BUFFER.lock().push(args.as_str().unwrap());
+    }
+
     pub mod init {
         pub fn preload() {}
         pub fn load() {}
     }
+
     pub mod power {
         pub fn halt() -> ! {
             loop {}
